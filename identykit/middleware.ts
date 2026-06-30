3ebc33ed5e2765e1d5cb62268b56f7297fa989f9
turbillon50 <turbillon50@gmail.com>
2026-06-30 15:50:34 -0500

    fix: middleware clerk edge compatible"

--- identykit/middleware.ts
+++ identykit/middleware.ts
@@ -13,5 +13,8 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)","/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
